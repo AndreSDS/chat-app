87411a675faf101b401ff5adf4dd4d6b88dfc722
andresds <andre.souza@asapcred.com.br>
2025-02-11 18:08:51 -0300

    Add header with navigation links to layout
    - Import Link component from Next.js
    - Add header with Chat and About navigation links
    - Apply styling to header and layout
    - Wrap children in flex container for responsive layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <main>
+      <h1 className="text-4l font-bold">About Us</h1>
+      <div className="text-4l">
+        About Us And our Awesome Application
+      </div>
+    </main>
+  );
+}
+ 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-5`}
       >
-        {children}
+        <header
+          className="text-white font-bold bg-green-900 text-2xl px-2 rounded-lg rounded-t-none
+          shadow-md shadow-blue-300
+        "
+        >
+          <div className="flex flex-row">
+            <Link href="/">Chat</Link>
+            <Link href="/about" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
       </body>
     </html>
   );
